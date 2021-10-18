ccd8528504c1bb42af95f692ebd660b0864c8268
DmitryDalmar <dmitriy@zvonov.xyz>
2021-10-18 16:25:55 +0300

    try to add async methods

--- lib/page/todo.dart
+++ lib/page/todo.dart
@@ -4,7 +4,38 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class Todo extends StatefulWidget {
-  const Todo({var key}): super(key: key);
+  const Todo({var key}) : super(key: key);
+
+  final String serverHost = 'http://ooake9xses.laravel-sail.site:8080';
+
+  Future<List> getTasks() async {
+    todoList = [];
+
+    var response =
+    await http.get(Uri.parse(serverHost + '/api/task'), headers: {
+      'Accept': 'application/json',
+    });
+
+    var result = jsonDecode(response.body);
+
+    result['data'].forEach((element) {
+      todoList.add(element);
+    });
+
+    return todoList;
+  }
+
+  void addTask(task) async {
+    var response = await http.post(Uri.parse(serverHost + '/api/task'),
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: task);
+
+    var result = jsonDecode(response.body)['data'];
+
+    // todoList.add(result);
+  }
 
   @override
   State<Todo> createState() => _TodoState();
@@ -14,25 +45,15 @@ class _TodoState extends State<Todo> {
   List todoList = [];
   String userToDo = '';
 
-  void getTasks() async {
-    var response = await http.get(Uri.parse('http://r9wnexeh1u.laravel-sail.site:8080/api/task'), headers: {
-      'Accept': 'application/json',
-    });
-
-    var result = jsonDecode(response.body);
-
-    result['data'].forEach((element) {
-      element['uniqueKey'] = UniqueKey();
-
-      todoList.add(element);
-    });
-  }
-
   @override
   void initState() {
     super.initState();
 
     this.getTasks();
+  }
+
+  void setToDoList(listData) {
+    todoList = listData;
   }
 
   @override
@@ -43,11 +64,11 @@ class _TodoState extends State<Todo> {
         title: const Text('Распил налогов'),
         centerTitle: true,
       ),
-      body: ListView.builder(
+      /*body: ListView.builder(
           itemCount: todoList.length,
           itemBuilder: (BuildContext context, int index) {
             return Dismissible(
-                key: Key(todoList[index]['uniqueKey'].toString()),
+                key: Key(todoList[index]['id'].toString()),
                 child: Card(
                   child: ListTile(
                       title: Text(todoList[index]['title']),
@@ -70,30 +91,67 @@ class _TodoState extends State<Todo> {
                 },
             );
           }
+      ),*/
+      body: FutureBuilder<List>(
+        future: getTasks(),
+        builder: (context, snapshot) {
+          // operation for completed state
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: todoList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Dismissible(
+                    key: Key(todoList[index]['id'].toString()),
+                    child: Card(
+                      child: ListTile(
+                        title: Text(todoList[index]['title']),
+                        trailing: IconButton(
+                          icon: const Icon(
+                            Icons.delete,
+                          ),
+                          onPressed: () => {
+                            setState(() {
+                              todoList.removeAt(index);
+                            })
+                          },
+                        ),
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                    },
+                  );
+                });
+          }
+          // spinner for uncompleted state
+          return Text('text');
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          showDialog(context: context, builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Добавить налог'),
-              content: TextField(
-                onChanged: (String value) {
-                  userToDo = value;
-                }
-              ),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() => {
-                      todoList.add({'title': userToDo, 'uniqueKey': UniqueKey()})
-                    });
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Поставить народ на счетчик'),
-                )
-              ],
-            );
-          });
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Добавить налог'),
+                  content: TextField(onChanged: (String value) {
+                    userToDo = value;
+                  }),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() => {
+                              addTask({'title': userToDo})
+                            });
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Поставить народ на счетчик'),
+                    )
+                  ],
+                );
+              });
         },
         child: const Icon(
           Icons.add,
